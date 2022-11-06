29379ae06bf26cb062dea07e1752742b77e15c13
darkmooninja <darkmooninja3481@gmail.com>
2022-11-06 00:48:47 -0700

    Template.java
    Replaced Template to a more understandable template

--- Template.java
+++ Template.java
@@ -1,14 +1,17 @@
-package Template; //remove or comment this line when submitting
 import java.io.*;
 import java.util.*;
 
 public class Template {
+        //Remember to change the name above to the respective file name
 public static void main(String[] args) throws IOException {
-        BufferedReader r = new BufferedReader(new FileReader("Template.in")); //Make sure to get the specific path and change back when submitting
+        BufferedReader r = new BufferedReader(new FileReader("Template.in"));
+        //Make sure to get the specific path when testing
+        //Change back to the file name provided by USACO when submitting(Older versions of tests)
+        //Replace "new FileReader("Template.in")" to "new InputStreamReader(System.in)" when submitting (New version of tests using Stdin)
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("Template.out")));
         StringTokenizer st = new StringTokenizer(r.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(r.readLine());
+        int n = Integer.parseInt(st.nextToken()); //Changes the next token into an Integer. Use "st.nextToken()" by itself to pull a string
+        st = new StringTokenizer(r.readLine()); //Reads the next line
 
 	    	
 //      pw.println(<Answer>);    How to submit/Change to Sysout to test
